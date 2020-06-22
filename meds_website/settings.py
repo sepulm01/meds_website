2ed03b24405881bc5687829fa950743b8073bc8a
sepulm01 <rguax910>
2020-06-22 08:29:35 -0400

    3 com

--- meds_website/settings.py
+++ meds_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b6%5_v5sqv(^tkg%2)7xon98w)s3(5t9yf6xn__k25)f0hceck'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['meds.streetflow.cl']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/meds_website/static/'
